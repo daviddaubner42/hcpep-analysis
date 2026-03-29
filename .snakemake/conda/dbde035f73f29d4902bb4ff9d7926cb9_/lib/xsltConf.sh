b68da97d0e08ac9263473ba9e5f53da68ce448f5
daviddaubner42 <davidaubner@gmail.com>
2026-03-29 22:45:09 +0200

    Init

---
+++ .snakemake/conda/dbde035f73f29d4902bb4ff9d7926cb9_/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/brainsimulation/Desktop/hcpep-analysis/.snakemake/conda/dbde035f73f29d4902bb4ff9d7926cb9_/lib"
+XSLT_LIBS="-lxslt -L/Users/brainsimulation/Desktop/hcpep-analysis/.snakemake/conda/dbde035f73f29d4902bb4ff9d7926cb9_/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/brainsimulation/Desktop/hcpep-analysis/.snakemake/conda/dbde035f73f29d4902bb4ff9d7926cb9_/include"
+MODULE_VERSION="xslt-1.1.43"
