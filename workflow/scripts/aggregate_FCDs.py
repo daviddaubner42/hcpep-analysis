fca15aacd9136d80e1314b8667364f257bcb4ee6
daviddaubner42 <davidaubner@gmail.com>
2026-05-28 12:20:08 +0200

    Fix

--- workflow/scripts/aggregate_FCDs.py
+++ workflow/scripts/aggregate_FCDs.py
@@ -10,7 +10,28 @@ parser.add_argument("--fcds_path", type=str, help="The path to the FCD results d
 parser.add_argument("--subids", type=str, nargs="+", help="All the subids to be included")
 parser.add_argument("--thread", type=str)
 parser.add_argument("--demo_data_path", type=str, help="Path to the demographic data")
+parser.add_argument("--motion_summary", type=str)
 args = parser.parse_args()
+
+# Find excluded participants for this thread
+motion = pd.read_csv(args.motion_summary, sep="\t")
+
+excluded = []
+
+thread_ses = args.thread.split("_")[0].split("-")[1]
+thread_dir = args.thread.split("_")[1].split("-")[1]
+for i in range(len(motion)):
+    subid = motion.loc[i, "subid"]
+    ses = motion.loc[i, "ses"]
+    dir = motion.loc[i, "dir"]
+    max_fds_trans = motion.loc[i, "max_fd_trans"]
+    max_fds_rot = motion.loc[i, "max_fd_rot"]
+    if max_fds_trans > 3 or max_fds_rot > 3:
+        if ses == thread_ses:
+            if dir == thread_dir:
+                excluded.append(int(subid))
+
+subids = [subid for subid in args.subids if subid not in excluded]
 
 # Load demographic data
 demo_data = pd.read_table(args.demo_data_path)
@@ -20,7 +41,7 @@ fcds = {}
 hists = {}
 
 # Create the confounds matrix and load the FCD results
-for subid in args.subids:
+for subid in subids:
     cur_sub = demo_data.loc[demo_data["src_subject_id"] == subid]
     confounds.append([int(cur_sub.phenotype.item() == "Patient"), int(cur_sub.interview_age.item()), int(cur_sub.sex.item() == 'F')])
     
@@ -40,7 +61,7 @@ for bin in range(all_hists.shape[1]):
     target = all_hists[:, bin]
     reg = LinearRegression().fit(confounds, target)
 
-    for i, subid in enumerate(args.subids):
+    for i, subid in enumerate(subids):
         hists[subid][bin] -= confounds[i, 1]*reg.coef_[1] + confounds[i, 2]*reg.coef_[2]
 
 # Save regressed histograms
